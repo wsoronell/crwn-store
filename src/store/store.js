709666a9361ceb692e5654740d11c75d2900d86d
wsoronell <wsoronell@gmail.com>
2022-05-26 09:59:35 -0400

    added redux-persist and cleaned disabled redux-logger when in production environment. This completes section 15. The next section is section 16 lesson 168

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,22 @@
 import logger from "redux-logger";
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
 import { rootReducer } from "./root-reducer";
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+};
+
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-    reducer: rootReducer,
-    middleware: [logger]
+    reducer: persistedReducer,
+    middleware: middleWares
 });
+
+export const persistor = persistStore(store);
